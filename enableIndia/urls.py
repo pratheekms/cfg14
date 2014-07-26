c7a7420c9af9dfc262a57ebcd78be38e1eafb71c
pratheek ms <pratheekonline@gmail.com>
2014-07-26 19:29:06 +0530

    login

--- enableIndia/urls.py
+++ enableIndia/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'enableIndia.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^pdp/', include('pdp.urls')),
+    url(r'^$', 'pdp.views.login'),
 )
--- pdp/models.py
+++ pdp/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Mentor(models.Model):
+	user = models.OneToOneField(User)
+	approved = models.BooleanField(default = False)
+	rating = models.PositiveIntegerField(default = 0, validators = [MaxValueValidator(10), MinValueValidator(100)])
+	# add additional fields if required
+
+	def __unicode__(self):
+		return self.user.username
+
+
+class Mentee(models.Model):
+	user = models.OneToOneField(User)
+	# add additional fields if required
+
+	def __unicode__(self):
+		return self.user.username
+
+class Moderator(models.Model):
+	user = models.OneToOneField(User)
+	# add additional fields if required
+
+	def __unicode__(self):
+		return self.user.username
+
+
+class Admin(models.Model):
+	user = models.OneToOneField(User)
+	# add additional fields if required
+
+	def __unicode__(self):
+		return self.user.username
+
+class Skills(models.Model):
+	category_list = (
+		('tech','Technology'),
+		('eng_speak','English speaking'),
+		)
+	category = models.CharField(max_length = 30, choices = category_list, default = 'Select a category')
+	sub_category = models.TextField()
